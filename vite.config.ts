027c2ecfbd7d09897e6cd3ca65b664af77900159
Bartosz Wojtczak <bartoszwojtczak99@gmail.com>
2024-11-11 13:01:18 +0100

    package building css

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
 	plugins: [
 		react(),
+		cssInjectedByJsPlugin(),
 		dts({
 			insertTypesEntry: true,
 			include: ["src/**/*.ts", "src/**/*.tsx"],
